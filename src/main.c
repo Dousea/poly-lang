6f0202be8b214804194dfd29348f5eab217956c4
Orlando Hutapea <Orlando Hutapea>
2016-12-23 11:08:02 +0900

    Function changed from polyCompile to polyInterpret

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main()
 	// A test..
 	PolyVM *vm = polyNewVM();
 
-	polyCompile(&vm->compiler, "if a + b == c:\n    output()");
+	polyInterpret(vm, "if a + b == c:\n    output()");
 	polyFreeVM(vm);
 
 	return 0;
