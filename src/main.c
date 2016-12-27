208c9e2b387f20ccbcfe46a478fed3c548fc03d7
Orlando Hutapea <Orlando Hutapea>
2016-12-27 12:37:49 +0900

    Changed the testing source code

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main()
 	// A test..
 	PolyVM *vm = polyNewVM();
 
-	polyInterpret(vm, "if a + b == c:\n    output()");
+	polyInterpret(vm, "if a + b == c:\n\toutput()\n\t\toutput()\n\t\t\toutput()");
 	polyFreeVM(vm);
 
 	return 0;
