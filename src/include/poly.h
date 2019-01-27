52c82ed58b39c99e8faeb7aea0c56cd228609802
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-01-27 23:40:37 +0700

    Declared API function prototypes

--- src/include/poly.h
+++ src/include/poly.h
@@ -4,4 +4,10 @@
 typedef struct Config    PolyConfig;
 typedef struct Allocator PolyAllocator;
 typedef struct VM        PolyVM;
+
+void    polyInitConfig(PolyConfig *config);
+PolyVM* polyNewVM(PolyConfig *config);
+void    polyFreeVM(PolyVM *vm);
+void    polyInterpret(PolyVM *vm, const char *source);
+
 #endif
